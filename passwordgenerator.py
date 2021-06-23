60eff7c057168bc803a8cb59c9ffc49cee28dac7
Francesco <68869617+elfranches@users.noreply.github.com>
2021-06-23 10:44:22 -0300

    Update passwordgenerator.py
    Translated to english

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -5,5 +5,5 @@ def get_secure_random_string(length):
     secure_str = ''.join((secrets.choice(string.ascii_letters + string.digits) for i in range(length)))
     return secure_str
 
-print("Primer Token seguro de 8 digitos:", get_secure_random_string(8))
-print("Segundo Token seguro de 10 digitos:", get_secure_random_string(10))
+print("First 8 digit token: ", get_secure_random_string(8))
+print("Second 10 digit token: ", get_secure_random_string(10))
